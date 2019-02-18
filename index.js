9314f8537a81aefd241524b9b702590da76e2926
Hicham Bezzizi <hicham_bezzizi@hotmail.com>
2019-02-18 09:14:57 +0100

    .updated.

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require("express"); // import the NPM dependancy package
+
+const bodyParser  = require("body-parser"); // parse body of incoming json requests
+
+const app     = express(); // initialise express() inside and write to the app variable
+
+app.use(bodyParser.json());
+
+require("./routes/usersRoutes")(app); // import route module and pass your app
+
+const PORT    = 5000; // choose what port on which to run the server
+
+app.listen(PORT, () => {   // use the app variable and listen on the port
+
+  console.log(`Server running`); 
+
+});
+
+
+
